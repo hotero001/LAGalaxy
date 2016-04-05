dffa493d212bfd2d84bd6b594c015213029095d3
Hector Otero <otero111@hotmail.com>
2016-04-04 18:58:19 -0700

    added improved image quality

--- LAGalaxy/ViewController.swift
+++ LAGalaxy/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "logo.png")!)
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "rsz_11logo.png")!)
     }
 
     override func didReceiveMemoryWarning() {
